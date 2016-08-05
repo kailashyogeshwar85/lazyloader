9c4be70c536e490117116ac252aa6a3d39b4ca87
kailashyogweshwar85 <kailash.yogeshwar@outlook.com>
2016-08-05 11:56:51 +0530

    Initial Push For Lazy Loader Plugin

---
+++ index.js
@@ -0,0 +1,46 @@
+
+
+"use strict";
+
+var contentstack =  require('contentstack-express');
+var ColorThief   = require("color-thief");
+var fs           = require("fs");
+var path         = require("path");
+var colorThief   = new ColorThief();
+
+module.exports = function Lazyloader() {
+
+    var options = Lazyloader.options;
+
+
+   Lazyloader.templateExtends = function(engine) {
+      engine.getEnvironment().addFilter("imagecolor", function(url) {
+          return getImageRGB(url);
+      });
+   };
+
+
+   Lazyloader.serverExtends = function(app) {
+   };
+
+   Lazyloader.beforePublish = function (data, next) {
+       next();
+   };
+
+   Lazyloader.beforeUnpublish = function (data, next) {
+       next();
+   };
+
+  var getImageRGB = function (url){
+      var _contentPath = null;
+      var env = contentstack.config._config.environment;
+      if(env == 'development'){
+        _contentPath =  path.join(process.cwd(),"/_content/en-us", decodeURI(url));
+      } else {
+        _contentPath =  path.join(process.cwd(),"/../_content/en-us", decodeURI(url));
+      }
+      var image  = fs.readFileSync(_contentPath);
+      var rgb    = colorThief.getColor(image);
+      return "style=background:rgb(" + rgb[0] + "," + rgb[1] + "," + rgb[2] + ")";
+  }
+};
